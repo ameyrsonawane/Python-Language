73cc5a4147b2dfaf438b461bbe5fa1f451641d23
Amey <amey.sonawane1@gmail.com>
2022-10-06 15:52:01 +0530

    2nd commit

--- Basics/Operators.py
+++ Basics/Operators.py
@@ -1,8 +1,81 @@
-# Arithmatic operators (+ - * /)
-x=15
+# Arithmatic Operators (+ - * /)
+
+'''x = 15
 y=7
 print(x+y)
 print(x*y)
 print(x**y) # Exponenciation
-print(x//y) # Result will be near to whole number
+print(x//y) # Result will be near to whole number'''
+
+
+# Assignment Operators
+
+'''x = 10
+print(x)
+x += 3
+print(x)
+x -= 5
+print(x)
+x *= 5
+print(x)
+x /= 2
+print(x)
+x **= 3
+print(x)'''
+
+# Comparison Operators
+
+'''x = 22
+y = 11
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)'''
+
+# Logical Operators
+
+'''x = 10
+y = 4
+print(x == y and x > y  # both condition should be satisfied
+print(x == y or x > y)  # any one condition should be satisfied'''
+
+# Membership Operators
+
+'''x =["Amey","Sonawane"]
+y =["Amey","Sonawane"]
+
+print("Amey" in y)
+print("Amey" not in y)'''
+
+
+'''x = 19
+y = 3
+z = x + y
+print(z)
+z = x * y
+print(z)
+z = z + 5
+print(z)
+z = z + 3
+print(-z)'''
+
+# Operator Precedence
+
+'''print(5 + 8 - 4) # Follow BOD MOS Rule
+print(5 +(8-4))
+print(4 ** 2 + 4 - 2)'''
+
+
+# Boolean Datatype (True and False)
+
+x = 5
+y = 7
+z = "" # (Empty String)
+print(x == y)
+print(x < y)
+print(bool(z)) # bool function allows us to evaluate any of the values and return boolean datatype (True/False)
+
 
