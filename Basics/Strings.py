6d53a0b19eccaff1236dc63fdf4e58815891b9e2
Amey <amey.sonawane1@gmail.com>
2022-10-06 17:17:30 +0530

    3rd commit

---
+++ Basics/Strings.py
@@ -0,0 +1,25 @@
+# Datatypes in Python - 1) Integer 2) Floating Numbers 3) String 4) Boolean values 5) Tuple 6) Array 7) Dictionary
+
+# String-Sting is a datatype, it's sequence of characters enclosed in quotes. (always start from 0 index)
+# For single line string we use ' ' / " "
+# For multiline string we use '''  ''' / """  """
+
+
+'''x = "Welcome into the Object Oriented Language-Python"
+y = 'Learn all those things which are available in this language'
+print(x)
+print(y)
+print(x[1]) # to display particular character'''
+
+'''x = "Please do 'practice' on daily basis to learn me"
+y = 'Otherwise it will be "difficult" to understand what exactly I am'
+print(x)
+print(y)'''
+
+z = '''This is the multiline 
+sting'''
+print(z)
+print("is" in z) # Membership Operator
+print("it's" in z) # Membership Operator
+
+
