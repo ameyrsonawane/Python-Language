ca6960f9fcc83718b89f7ff9c2ed4b206865f608
Amey <amey.sonawane1@gmail.com>
2022-10-10 15:13:33 +0530

    10th commit

---
+++ Basics/.INDEX.py
@@ -0,0 +1,22 @@
+# 1) Variables
+# 2) Operators
+# 3) String
+# 4) Lists
+# 5) Sets
+# 6) Tuples
+# 7) Dictionaries
+# 8) If else
+# 9) Loops
+# 10) Function
+# 11) Keywords and Arguments
+# 12) Variables
+# 13) Variable Scope
+# 14) Date time module
+# 15) Classes and Object
+# 16) Class variables and Instance Variables
+# 17) Exception Handling
+# 18) How to Read and Write the files.
+# 19) How to Read and Write the data from excel.
+# 20) Difference between Lists,Array and Tuple
+# 21) Opps Concepts
+
